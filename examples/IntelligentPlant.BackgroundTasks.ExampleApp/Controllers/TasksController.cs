63d1f31503697fbc6f99eaa625138fa14dab69b9
Graham Watts <grahamcharleswatts@gmail.com>
2021-03-20 16:51:13 +0200

    Update TasksController.cs
    Allow tasks to periodically fail (so that event counters can show both successful and failed tasks)

--- examples/IntelligentPlant.BackgroundTasks.ExampleApp/Controllers/TasksController.cs
+++ examples/IntelligentPlant.BackgroundTasks.ExampleApp/Controllers/TasksController.cs
@@ -11,6 +11,8 @@ namespace IntelligentPlant.BackgroundTasks.ExampleApp.Controllers {
 
         private readonly ILogger<TasksController> _logger;
 
+        private static readonly Random s_rnd = new Random();
+
 
         public TasksController(IBackgroundTaskService backgroundTaskService, ILogger<TasksController> logger) {
             _backgroundTaskService = backgroundTaskService;
@@ -22,7 +24,12 @@ namespace IntelligentPlant.BackgroundTasks.ExampleApp.Controllers {
         [Route("create-task")]
         public Guid CreateTask() {
             return _backgroundTaskService.QueueBackgroundWorkItem(
-                ct => _logger.LogInformation("[BACKGROUND TASK] Running background task"),
+                ct => {
+                    _logger.LogInformation("[BACKGROUND TASK] Running background task");
+                    if (s_rnd.Next(101) < 30) {
+                        throw new Exception("Failed");
+                    }
+                },
                 "Example task"
             );
         }
