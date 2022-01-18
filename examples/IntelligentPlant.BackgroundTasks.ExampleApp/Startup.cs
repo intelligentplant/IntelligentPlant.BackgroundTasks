07a00af9505a2347b1155fae3725aebbe0ab762e
Graham Watts <grahamcharleswatts@gmail.com>
2022-01-18 09:31:33 +0200

    Add metric export to example app

--- examples/IntelligentPlant.BackgroundTasks.ExampleApp/Startup.cs
+++ examples/IntelligentPlant.BackgroundTasks.ExampleApp/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
@@ -20,14 +21,30 @@ namespace IntelligentPlant.BackgroundTasks.ExampleApp {
             services.AddAspNetCoreBackgroundTaskService();
             services.AddControllers();
 
+            var resourceBuilder = ResourceBuilder.CreateEmpty().AddService(GetType().Assembly.GetName().Name, autoGenerateServiceInstanceId: false);
+
             services.AddOpenTelemetryTracing(builder => {
                 builder
-                    .SetResourceBuilder(ResourceBuilder.CreateEmpty().AddService(GetType().Assembly.GetName().Name, autoGenerateServiceInstanceId: false))
+                    .SetResourceBuilder(resourceBuilder)
                     .AddAspNetCoreInstrumentation()
                     .AddSource(nameof(Controllers.TasksController))
                     .AddConsoleExporter();
             });
+
+            services.AddOpenTelemetryMetrics(builder => {
+                builder
+                    .SetResourceBuilder(resourceBuilder)
+                    .AddMeter(BackgroundTaskService.DiagnosticsSourceName)
+                    .AddConsoleExporter(options => {
+                        options.MetricReaderType = MetricReaderType.Periodic;
+                        options.AggregationTemporality = AggregationTemporality.Cumulative;
+                        options.PeriodicExportingMetricReaderOptions = new PeriodicExportingMetricReaderOptions() {
+                            ExportIntervalMilliseconds = 5000
+                        };
+                    });
+            });
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
